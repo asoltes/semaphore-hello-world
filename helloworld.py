c48b1300d824ad3b61ded17a48bb3605fae9944c
asoltes <89505208+asoltes@users.noreply.github.com>
2024-07-18 23:14:09 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
-def hello_world_devops():
-    print("Hello, world! Greetings to all the DevOps professionals!")
+import sys
+
+def hello_world_devops(company_name):
+    print(f"Hello, world! Greetings to all the DevOps professionals at {company_name}!")
 
 if __name__ == "__main__":
-    hello_world_devops()
+    if len(sys.argv) != 2:
+        print("Usage: python hello_world_devops.py <company_name>")
+    else:
+        company_name = sys.argv[1]
+        hello_world_devops(company_name)
