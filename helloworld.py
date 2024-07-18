7b2dc1fd2a866334748891a42512497d797e8661
asoltes <89505208+asoltes@users.noreply.github.com>
2024-07-18 23:25:09 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 def hello_world_devops():
-    print("Hello, world! Greetings to all the DevOps professionals!")
+    print("Hello, world! This is a test")
 
 if __name__ == "__main__":
     hello_world_devops()
