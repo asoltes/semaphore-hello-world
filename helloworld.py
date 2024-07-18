98d2162dbc83f604df28649e1d9ed312da153caa
asoltes <89505208+asoltes@users.noreply.github.com>
2024-07-18 23:30:54 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,12 @@
-def hello_world_devops():
-    print("Hello, world! This is a test")
+import argparse
+
+def hello_world_devops(company_name):
+    print(f"Hello, world! Greetings to all the DevOps professionals at {company_name}!")
 
 if __name__ == "__main__":
-    hello_world_devops()
+    parser = argparse.ArgumentParser(description='Send greetings to DevOps professionals at a company.')
+    parser.add_argument('--company', type=str, required=True, help='The name of the company')
+    
+    args = parser.parse_args()
+    
+    hello_world_devops(args.company)
